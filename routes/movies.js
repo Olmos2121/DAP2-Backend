aba2c060412b2d50a62695e2b6c144d80273a32b
Olmos2121 <tomyolmos1@hotmail.com>
2025-09-16 02:33:42 -0300

    Swagger y endpoints configurados correctamente

--- routes/movies.js
+++ routes/movies.js
@@ -3,12 +3,92 @@ const router = express.Router();
 const controller = require('../controllers/moviesController');
 
 // CRUD Películas
-router.post('/', controller.createMovie);
-router.get('/search', controller.searchMovies);
-router.get('/genre/:genre', controller.getMoviesByGenre);
-router.get('/:id', controller.getMovie);
-router.get('/', controller.getAllMovies);
-router.put('/:id', controller.updateMovie);
-router.delete('/:id', controller.deleteMovie);
+router.post('/', 
+    /*
+      #swagger.tags = ['Movies']
+      #swagger.summary = 'Crear película'
+      #swagger.description = 'Crea una nueva película.'
+      #swagger.consumes = ['application/json']
+      #swagger.produces = ['application/json']
+      #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: { $ref: '#/definitions/MovieCreateInput' }
+      }
+      #swagger.responses[201] = { description: 'Creado', schema: { $ref: '#/definitions/Movie' } }
+      #swagger.responses[400] = { description: 'Bad Request' }
+    */
+    controller.createMovie
+);
+
+router.get('/search', 
+    /*
+      #swagger.tags = ['Movies']
+      #swagger.summary = 'Buscar películas'
+      #swagger.description = 'Busca películas por título o descripción.'
+      #swagger.parameters['query'] = { in: 'query', required: true, type: 'string' }
+      #swagger.responses[200] = { description: 'OK', schema: { type: 'array', items: { $ref: '#/definitions/Movie' } } }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.searchMovies
+);
+
+router.get('/genre/:genre', 
+    /*
+      #swagger.tags = ['Movies']
+      #swagger.summary = 'Obtener películas por género'
+      #swagger.parameters['genre'] = { in: 'path', required: true, type: 'string' }
+      #swagger.responses[200] = { description: 'OK', schema: { type: 'array', items: { $ref: '#/definitions/Movie' } } }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.getMoviesByGenre
+);
+
+router.get('/:id', 
+    /*
+      #swagger.tags = ['Movies']
+      #swagger.summary = 'Obtener película por ID'
+      #swagger.parameters['id'] = { in: 'path', required: true, type: 'integer' }
+      #swagger.responses[200] = { description: 'OK', schema: { $ref: '#/definitions/Movie' } }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.getMovie
+);
+
+router.get('/', 
+    /*
+      #swagger.tags = ['Movies']
+      #swagger.summary = 'Obtener todas las películas'
+      #swagger.responses[200] = { description: 'OK', schema: { type: 'array', items: { $ref: '#/definitions/Movie' } } }
+    */
+    controller.getAllMovies
+);
+
+router.put('/:id', 
+    /*
+      #swagger.tags = ['Movies']
+      #swagger.summary = 'Actualizar película por ID'
+      #swagger.parameters['id'] = { in: 'path', required: true, type: 'integer' }
+      #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: { $ref: '#/definitions/MovieUpdateInput' }
+      }
+      #swagger.responses[200] = { description: 'OK', schema: { $ref: '#/definitions/Movie' } }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.updateMovie
+);
+
+router.delete('/:id', 
+    /*
+      #swagger.tags = ['Movies']
+      #swagger.summary = 'Eliminar película por ID'
+      #swagger.parameters['id'] = { in: 'path', required: true, type: 'integer' }
+      #swagger.responses[204] = { description: 'Sin Contenido' }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.deleteMovie
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,81 @@ const router = express.Router();
 const controller = require('../controllers/userController');
 
 // CRUD Usuarios
-router.post('/', controller.createUser);
-router.get('/search', controller.getUserByEmail);
-router.get('/:id', controller.getUser);
-router.get('/', controller.getAllUsers);
-router.put('/:id', controller.updateUser);
-router.delete('/:id', controller.deleteUser);
+router.post('/',  
+    /*
+      #swagger.tags = ['Users']
+      #swagger.summary = 'Crear usuario'
+      #swagger.description = 'Crea un nuevo usuario.'
+      #swagger.consumes = ['application/json']
+      #swagger.produces = ['application/json']
+      #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: { $ref: '#/definitions/UserCreateInput' }
+      }
+      #swagger.responses[201] = { description: 'Creado', schema: { $ref: '#/definitions/User' } }
+      #swagger.responses[400] = { description: 'Bad Request' }
+    */
+    controller.createUser
+);
+
+router.get('/search', 
+    /*
+      #swagger.tags = ['Users']
+      #swagger.summary = 'Buscar usuario por email'
+      #swagger.description = 'Busca un usuario por su dirección de email.'
+      #swagger.parameters['email'] = { in: 'query', required: true, type: 'string' }
+      #swagger.responses[200] = { description: 'OK', schema: { $ref: '#/definitions/User' } }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.getUserByEmail
+);
+
+router.get('/:id', 
+    /*
+      #swagger.tags = ['Users']
+      #swagger.summary = 'Obtener usuario por ID'
+      #swagger.parameters['id'] = { in: 'path', required: true, type: 'integer' }
+      #swagger.responses[200] = { description: 'OK', schema: { $ref: '#/definitions/User' } }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.getUser
+);
+
+router.get('/', 
+    /*
+      #swagger.tags = ['Users']
+      #swagger.summary = 'Obtener todos los usuarios'
+      #swagger.responses[200] = { description: 'OK', schema: { type: 'array', items: { $ref: '#/definitions/User' } } }
+    */
+    controller.getAllUsers
+);
+
+router.put('/:id', 
+    /*
+      #swagger.tags = ['Users']
+      #swagger.summary = 'Actualizar usuario por ID'
+      #swagger.parameters['id'] = { in: 'path', required: true, type: 'integer' }
+      #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: { $ref: '#/definitions/UserUpdateInput' }
+      }
+      #swagger.responses[200] = { description: 'OK', schema: { $ref: '#/definitions/User' } }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.updateUser
+);
+
+router.delete('/:id', 
+    /*
+      #swagger.tags = ['Users']
+      #swagger.summary = 'Eliminar usuario por ID'
+      #swagger.parameters['id'] = { in: 'path', required: true, type: 'integer' }
+      #swagger.responses[204] = { description: 'Sin Contenido' }
+      #swagger.responses[404] = { description: 'No encontrado' }
+    */
+    controller.deleteUser
+);
 
 module.exports = router;
